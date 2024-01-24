4f70fb110c189db48428cc53dfae9787d5368da8
viktor <victor@gmail.com>
2024-01-24 19:21:58 +0400

    Initial Commit

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Victor Tormyshev on 24.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
